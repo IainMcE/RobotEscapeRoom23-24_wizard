f73bf0e3e634314c36022b3e8cbbd4778e3fde63
ijmce <ijmcewen@wpi.edu>
2023-12-04 21:05:46 -0500

    Added <area> tags to the floor editor, allowing onclick events

--- js/roomLayout.js
+++ js/roomLayout.js
@@ -14,8 +14,6 @@ const context = canvas.getContext("2d");
 
 window.addEventListener('DOMContentLoaded', ()=>{
     newRoom();
-    validateWalls();
-    drawRoom();
 });
 
 function newRoom(){
@@ -80,6 +78,11 @@ function validateWalls(){
 function drawRoom(){
     validateWalls()
     context.clearRect(0,0, canvas.width, canvas.height)
+    map = document.getElementById("floorMap")
+    wallList = map.children
+    while(wallList.length>0){
+        wallList[0].remove()
+    }
     for(let i = 0; i<height; i++){
         for(let j = 0; j<width; j++){
             x = size*(j+1);
@@ -91,74 +94,78 @@ function drawRoom(){
                 if(room[i][j].walls[0]!==null){//top wall
                     let corner1x = x, corner2x = x+size;
                     if(room[i][j].walls[3]!==null){//left wall
-                        corner1x = x-(size*0.4);
+                        corner1x = x-(size*0.3);
                     }else if(i>0 && j>0){
                         if(room[i-1][j-1].walls[1]!==null){//top left space has right wall
-                            corner1x = x+(size*0.4);
+                            corner1x = x+(size*0.3);
                         }
                     }
                     if(room[i][j].walls[1]!==null){//right wall
-                        corner2x = x+(size*1.4);
+                        corner2x = x+(size*1.3);
                     }else if(i>0 && j<width-1){
                         if(room[i-1][j+1].walls[3]!==null){//top right space has left wall
-                            corner2x = x+(size*0.6);
+                            corner2x = x+(size*0.7);
                         }
                     }
-                    drawQuad(corner1x, y-(size*0.4), x, y, x+size, y, corner2x, y-(size*0.4))
+                    drawQuad(corner1x, y-(size*0.3), x, y, x+size, y, corner2x, y-(size*0.3))
+                    interactableQuad(map, j, i, "top", corner1x, y-(size*0.3), x, y, x+size, y, corner2x, y-(size*0.3))
                 }
                 if(room[i][j].walls[1]!==null){//right wall
                     let corner1y = y, corner2y = y+size;
                     if(room[i][j].walls[0]!==null){//top wall
-                        corner1y = y-(size*0.4);
+                        corner1y = y-(size*0.3);
                     }else if(i>0 && j<width-1){
                         if(room[i-1][j+1].walls[2]!==null){//top right space has bottom wall
-                            corner1y = y+(size*0.4);
+                            corner1y = y+(size*0.3);
                         }
                     }
                     if(room[i][j].walls[2]!==null){//bottom wall
-                        corner2y = y+(size*1.4);
+                        corner2y = y+(size*1.3);
                     }else if(i<height-1 && j<width-1){
                         if(room[i+1][j+1].walls[0]!==null){//bottom right space has top wall
-                            corner2y = y+(size*0.6);
+                            corner2y = y+(size*0.7);
                         }
                     }
-                    drawQuad(x+size, y+size, x+size, y, x+(size*1.4), corner1y, x+(size*1.4), corner2y)
+                    drawQuad(x+size, y+size, x+size, y, x+(size*1.3), corner1y, x+(size*1.3), corner2y)
+                    interactableQuad(map, j, i, "right", x+size, y+size, x+size, y, x+(size*1.3), corner1y, x+(size*1.3), corner2y)
                 }
                 if(room[i][j].walls[2]!==null){//bottom wall
                     let corner1x = x, corner2x = x+size;
                     if(room[i][j].walls[3]!==null){//left wall
-                        corner1x = x-(size*0.4);
+                        corner1x = x-(size*0.3);
                     }else if(i<height-1 && j>0){
                         if(room[i+1][j-1].walls[1]!==null){//bottom left space has right wall
-                            corner1x = x+(size*0.4);
+                            corner1x = x+(size*0.3);
                         }
                     }
                     if(room[i][j].walls[1]!==null){//right wall
-                        corner2x = x+(size*1.4);
+                        corner2x = x+(size*1.3);
                     }else if(i<height-1 && j<width-1){
                         if(room[i+1][j+1].walls[3]!==null){//bottom right space has left wall
-                            corner2x = x+(size*0.6);
+                            corner2x = x+(size*0.7);
                         }
                     }
-                    drawQuad(corner1x, y+(size*1.4), x, y+size, x+size, y+size, corner2x, y+(size*1.4))
+                    drawQuad(corner1x, y+(size*1.3), x, y+size, x+size, y+size, corner2x, y+(size*1.3))
+                    interactableQuad(map, j, i, "bottom", corner1x, y+(size*1.3), x, y+size, x+size, y+size, corner2x, y+(size*1.3))
                 }
                 if(room[i][j].walls[3]!==null){//left wall
                     let corner1y = y, corner2y = y+size;
                     if(room[i][j].walls[0]!==null){//top wall
-                        corner1y = y-(size*0.4);
+                        corner1y = y-(size*0.3);
                     }else if(i>0 && j>0){
                         if(room[i-1][j-1].walls[2]!==null){//top left space has bottom wall
-                            corner1y = y+(size*0.4);
+                            corner1y = y+(size*0.3);
                         }
                     }
                     if(room[i][j].walls[2]!==null){//bottom wall
-                        corner2y = y+(size*1.4);
+                        corner2y = y+(size*1.3);
                     }else if(i<height-1 && j>0){
                         if(room[i+1][j-1].walls[0]!==null){//bottom left space has top wall
-                            corner2y = y+(size*0.6);
+                            corner2y = y+(size*0.7);
                         }
                     }
-                    drawQuad(x, y+size, x, y, x-(size*0.4), corner1y, x-(size*0.4), corner2y)
+                    drawQuad(x, y+size, x, y, x-(size*0.3), corner1y, x-(size*0.3), corner2y)
+                    interactableQuad(map, j, i, "left", x, y+size, x, y, x-(size*0.3), corner1y, x-(size*0.3), corner2y)
                 }
             }
         }
@@ -175,19 +182,12 @@ function drawQuad(x1, y1, x2, y2, x3, y3, x4, y4){
     context.stroke();
 }
 
-function setRoom(){
-    room[0][0].floor="floor"
-    room[0][1].floor="floor"
-    room[0][2].floor="floor"
-    room[0][3].floor="floor"
-    room[1][2].floor="floor"
-    room[1][3].floor="floor"
-    room[2][0].floor="floor"
-    room[2][1].floor="floor"
-    room[3][0].floor="floor"
-    room[3][1].floor="floor"
-    room[3][2].floor="floor"
-    room[3][3].floor="floor"
+function interactableQuad(map, parentX, parentY, side, x1, y1, x2, y2, x3, y3, x4, y4){
+    wall = document.createElement("area")
+    wall.shape = "poly"
+    wall.coords = x1+","+y1+","+x2+","+y2+","+x3+","+y3+","+x4+","+y4
+    wall.addEventListener("click", ()=>{alert("Square "+parentX+", "+parentY+"'s "+side+" side clicked")})
+    map.append(wall)
 }
 
 function updateRoom(){
@@ -220,4 +220,12 @@ for(let i = 0; i<height; i++){
         floorCheck.addEventListener("click", updateRoom)
         area.appendChild(floorCheck)
     }
+}
+
+function selectAll(){
+    checks = document.getElementsByClassName("floorCheckbox")
+    for(let i = 0; i<checks.length; i++){
+        checks[i].checked = true;
+    }
+    updateRoom()
 }
